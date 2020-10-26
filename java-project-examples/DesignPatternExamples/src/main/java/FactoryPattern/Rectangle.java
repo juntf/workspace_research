6feb07a382086d0bdab65332e10f0d28cc585fb1
juntf <syunnlib@gmail.com>
2020-10-26 21:16:46 +0900

    reset projects

--- java-project-examples/DesignPatternExamples/src/main/java/FactoryPattern/Rectangle.java
+++
@@ -1,9 +0,0 @@
-package FactoryPattern;
-
-public class Rectangle implements Shape{
-
-    @Override
-    public void draw() {
-        System.out.println("Inside Reactangle::draw() method;");
-    }
-}
