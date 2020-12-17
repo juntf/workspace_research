06b7cd2b05968e7ab3175b90ceaaec174bfa50cb
이준 <4009795@qq.com>
2020-12-17 14:07:35 +0900

    BinaryHeap BFS

--- java-project-examples/DataStructureAndAlgorithmInJava/src/DS04P02Tree/BinTree_LinkedList.java
+++ java-project-examples/DataStructureAndAlgorithmInJava/src/DS04P02Tree/BinTree_LinkedList.java
@@ -1,2 +1,56 @@
-package DS04P02Tree;public class BinTree_LinkedList {
+package DS04P02Tree;
+
+import java.util.Iterator;
+
+// PDF-P144; Book-P132;
+
+public class BinTree_LinkedList implements BinaryTree {
+    protected BinaryTreePosition root;
+
+    public BinTree_LinkedList() {
+        this(null);
+    }
+
+    public BinTree_LinkedList(BinaryTreePosition r) {
+        root = r;
+    }
+    @Override
+    public BinaryTree getRoot() {
+        return root;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return null == root;
+    }
+
+    @Override
+    public int getSize() {
+        return isEmpty() ? 0 : root.getSize();
+    }
+
+    @Override
+    public int getHeight() {
+        return isEmpty() ? -1 : root.getHeight();
+    }
+
+    @Override
+    public Iterator elementsPreOrder() {
+        return root.elementsPreOrder();
+    }
+
+    @Override
+    public Iterator elementsInOrder() {
+        return root.elementsInOrder();
+    }
+
+    @Override
+    public Iterator elementsPostOrder() {
+        return root.elementsPostOrder();
+    }
+
+    @Override
+    public Iterator elementsLevelOrder() {
+        return root.elementsLevelOrder();
+    }
 }
--- java-project-examples/DataStructureAndAlgorithmInJava/src/DS04P02Tree/IteratorTree.java
+++ java-project-examples/DataStructureAndAlgorithmInJava/src/DS04P02Tree/IteratorTree.java
@@ -1,2 +1,46 @@
-package DS04P02Tree;public class IteratorTree {
+package DS04P02Tree;
+
+import DS02P03LinkedList.Position;
+import DS03P02List.List;
+
+import java.util.Iterator;
+
+
+public class IteratorTree implements Iterator {
+    private List list;
+    private Position nextPosition;
+
+    @Override
+    public boolean hasNext() {
+        return false;
+    }
+
+    @Override
+    public Object next() {
+        return null;
+    }
+
+    public IteratorTree() { list = null;}
+
+    // 前序遍历
+    public void elementsPreorderIterator(TreeLinkedList T) {
+        if(T == null) return ;
+        list.insertLast(T);
+        TreeLinkedList subTree = T.getFirstChild();
+        while(null != subTree) {
+            this.elementsPreorderIterator(subTree);
+
+            subTree = subTree.getNextSibling();
+        }
+    }
+
+    public void elementsPostorderIterator(TreeLinkedList T) {
+        if( null == T) return ;
+        TreeLinkedList subTree = T.getFirstChild();
+        while(null != subTree) {
+            this.elementsPostorderIterator(subTree);
+            subTree = subTree.getNextSibling();
+        }
+        list.insertLast(T);
+    }
 }
